397d951eb5c26a4989e00f79284a511d07cb7c9d
unknown <giancarmelo.pittala@gmail.com>
2023-09-14 09:34:03 +0200

    chore: add pinia

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    test
+    {{  testStore.text }}
+  </div>
+</template>
+
+
+<script setup>
+import { useTestStore } from '~/stores/test';
+const testStore = useTestStore()
+
+
+
+</script>
