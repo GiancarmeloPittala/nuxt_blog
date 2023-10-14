ed032ba50f2e7bbc6d5f880ef8d580819990e947
unknown <giancarmelo.pittala@gmail.com>
2023-10-14 10:38:05 +0200

    add home

---
+++ components/home/hero.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="grid grid-cols-2 gap-6">
+    <div>
+      <h1> lorem10 lorem10lorem10lorem10lorem10lorem10 </h1>
+    </div>
+    <div>
+      <h1> lorem10 </h1>
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
